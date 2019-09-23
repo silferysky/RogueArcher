3e328696e61b30c467f271f80e6eb30ba57457f9
Terence Chan <deathysayshi@gmail.com>
2019-09-24 04:08:08 +0800

    Elie's Event System
    Using function pointers for map instead of vector (One function will manage entire event list)

--- Scripts/TestSystem.h
+++ Scripts/TestSystem.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "Logger.h"
 #include "BaseSystem.h"
-#include "Event.h"
+#include "EventDispatcher.h"
 
 class TestSystem : public BaseSystem
 {
 public:
-	TestSystem(SYSTEMID id = (SYSTEMID)1) : BaseSystem(id) {}
-	virtual void Receive(const Event& event) { RE_CORE_INFO("TEST SYSTEM RECEIVED"); }
+	TestSystem(SYSTEMID id = (SYSTEMID)1) : BaseSystem(id) { /*eventDispatcher.AddListener(id, (LISTENER_HANDLER)Receive); */}
+	void Receive(const Event& ev) { RE_INFO(ev.ToString());  RE_CORE_INFO("TEST SYSTEM RECEIVED"); }
 };
