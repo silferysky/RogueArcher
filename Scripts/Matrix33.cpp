c3888f694d9c570331acec4e6c6b8ca816138968
UnGlame <35512452+UnGlame@users.noreply.github.com>
2019-09-26 05:59:56 +0800

    Errors (Missing files & extra files) fixed

--- Scripts/Matrix33.cpp
+++ Scripts/Matrix33.cpp
@@ -211,7 +211,7 @@ void Mtx33RotRad(Matrix3x3 &pResult, float angle)
 void Mtx33RotDeg(Matrix3x3 &pResult, float angle)
 {
 	// Convert angle to radian, then do rotation transformation in radians.
-	angle *= static_cast<float>(PI / 180);
+	angle *= static_cast<float>(RE_PI / 180);
 	Mtx33RotRad(pResult, angle);
 }
 
@@ -255,7 +255,7 @@ void Mtx33Inverse(Matrix3x3 &pResult, float &determinant, const Matrix3x3 &pMtx)
 	// Det = a(det12) - b(det02) + c(det01)
 	determinant = pMtx.m00 * det00 - pMtx.m01 * det01 + pMtx.m02 * det02;
 
-	if (determinant <= EPSILON && determinant >= -EPSILON)
+	if (determinant <= RE_EPSILON && determinant >= -RE_EPSILON)
 	{
 		pResult = NULL;
 		return;
