fe6002977af1bfcea4f46842a7316aac9970efcd
Loh Kai Yi <liverpool0_91@hotmail.com>
2019-09-29 22:56:02 +0800

    updated config

--- Scripts/Main.cpp
+++ Scripts/Main.cpp
@@ -121,6 +121,7 @@ HWND CreateOpenGLWindow(char* title, int x, int y, int width, int height,
 	return hWnd;
 }
 
+
 //Use for console
 int APIENTRY
 WinMain(HINSTANCE hCurrentInst, HINSTANCE hPreviousInst,
@@ -132,7 +133,6 @@ WinMain(HINSTANCE hCurrentInst, HINSTANCE hPreviousInst,
 	MSG   msg;				/* message */
 	REConfig config;
 	config.ConfigInit();
-
 	hWnd = CreateOpenGLWindow(const_cast<char*>("Rogue Engine"), config.GetX(), config.GetY(), config.GetWidth(), config.GetHeight(), config.GetByte(), config.GetFlags());
 	if (hWnd == NULL)
 		exit(1);
