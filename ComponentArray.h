624eeb32a61af173b2f5826db815adc0cf74a8d5
lohkaiyi <51329075+lohkaiyi@users.noreply.github.com>
2019-09-30 19:54:54 +0800

    asserts

---
+++ ComponentArray.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "EntityManager.h"
+#include <unordered_map>
+#include <cassert>
+class BaseComponentArray
+{
+public:
+	virtual ~BaseComponentArray() = default;
+	virtual void EntityDestroyed(Entity entity) = 0;
+};
+
+
+template<typename T>
+class ComponentArray : public BaseComponentArray
+{
+public:
+	void InsertData(Entity entity, T component)
+	{
+		assert(REEntityToIndexMap.find(entity) == REEntityToIndexMap.end() && "Component added to same entity more than once.");
+		// Put new entry at end and update the maps
+		size_t newIndex = RESize;
+		REEntityToIndexMap[entity] = newIndex;
+		REIndexToEntityMap[newIndex] = entity;
+		REComponentArray[newIndex] = component;
+		++RESize;
+	}
+
+	void RemoveData(Entity entity)
+	{
+		assert(REEntityToIndexMap.find(entity) != REEntityToIndexMap.end() && "Removing non-existent component.");
+		if (!RESize)
+			return;
+		// Copy element at end into deleted element's place to maintain density
+		size_t indexOfRemovedEntity = REEntityToIndexMap[entity];
+		size_t indexOfLastElement = RESize - 1;
+		REComponentArray[indexOfRemovedEntity] = REComponentArray[indexOfLastElement];
+
+		// Update map to point to moved spot
+		Entity entityOfLastElement = REIndexToEntityMap[indexOfLastElement];
+		REEntityToIndexMap[entityOfLastElement] = indexOfRemovedEntity;
+		REIndexToEntityMap[indexOfRemovedEntity] = entityOfLastElement;
+
+		REEntityToIndexMap.erase(entity);
+		REIndexToEntityMap.erase(indexOfLastElement);
+
+		--RESize;
+	}
+
+	T& GetData(Entity entity)
+	{
+		//assert(REEntityToIndexMap.find(entity) != REEntityToIndexMap.end() && "Retrieving non-existent component.");
+		return REComponentArray[REEntityToIndexMap[entity]];
+	}
+
+	void EntityDestroyed(Entity entity) override
+	{
+		if (REEntityToIndexMap.find(entity) != REEntityToIndexMap.end())
+		{
+			RemoveData(entity);
+		}
+	}
+
+private:
+
+	std::array<T, MAX_ENTITIES> REComponentArray;
+
+	std::unordered_map<Entity, size_t> REEntityToIndexMap;
+
+	std::unordered_map<size_t, Entity> REIndexToEntityMap;
+
+	size_t RESize;
+};
---
+++ ComponentManager.h
@@ -0,0 +1,109 @@
+#pragma once
+#include <unordered_map>
+#include "EntityManager.h"
+#include "ComponentArray.h"
+#include "ComponentList.h"
+#include "Logger.h"
+#include <cassert>
+
+class ComponentManager
+{
+public:
+	template<typename T>
+	void RegisterComponent()
+	{
+		const char* typeName = typeid(T).name();
+		//assert(REComponentTypes.find(typeName) == REComponentTypes.end() && "Registering component type more than once.");
+
+		// Add this component type to the component type map
+		REComponentTypes.insert({ typeName, RENextComponentType });
+		
+		std::stringstream out;
+		out.clear();
+		out.str("");
+		out << "Creating " << typeName << "s...";
+		RE_CORE_INFO(out.str());
+		
+		// Create a ComponentArray pointer and add it to the component arrays map
+		REComponentArrays.insert({ typeName, std::make_shared<ComponentArray<T>>() });
+		
+
+		out.clear();
+		out.str("");
+		out << "Array of " << MAX_ENTITIES << " " << typeName << "s created!";
+		RE_CORE_INFO(out.str());
+		
+		// Increment the value so that the next component registered will be different
+		++RENextComponentType;
+	}
+
+	template<typename T>
+	ComponentType GetComponentType()
+	{
+		const char* typeName = typeid(T).name();
+		//assert(REComponentTypes.find(typeName) != REComponentTypes.end() && "Component not registered before use.");
+		return REComponentTypes[typeName];
+	}
+
+	ComponentType GetComponentType(const char* typeName)
+	{
+		return REComponentTypes[typeName];
+	}
+
+	template<typename T>
+	void AddComponent(Entity entity, T component)
+	{
+		std::stringstream out;
+		out << "Entity " << entity << "'s " << typeid(T).name() << " added!";
+		RE_CORE_INFO(out.str());
+		GetComponentArray<T>()->InsertData(entity, component);
+	}
+	
+	template<typename T>
+	void RemoveComponent(Entity entity)
+	{
+		GetComponentArray<T>()->RemoveData(entity);
+	}
+
+	template<typename T>
+	T& GetComponent(Entity entity)
+	{
+		return GetComponentArray<T>()->GetData(entity);
+	}
+
+	void EntityDestroyed(Entity entity)
+	{
+		// Notify each component array that an entity has been destroyed
+		// If it has a component for that entity, it will remove it
+		for (auto const& pair : REComponentArrays)
+		{
+			auto const& component = pair.second;
+
+			component->EntityDestroyed(entity);
+			RE_CORE_INFO("Components Removed\n");
+		}
+	}
+
+	size_t Size() const
+	{
+		return RENextComponentType;
+	}
+
+private:
+
+	template<typename T>
+	std::shared_ptr<ComponentArray<T>> GetComponentArray()
+	{
+		
+		const char* typeName = typeid(T).name();
+		assert(REComponentTypes.find(typeName) != REComponentTypes.end() && "Component not registered before use.");
+		return std::static_pointer_cast<ComponentArray<T>>(REComponentArrays[typeName]);
+	}
+
+	ComponentType RENextComponentType{};
+
+	std::unordered_map<const char*, ComponentType> REComponentTypes{};
+
+	std::unordered_map<const char*, std::shared_ptr<BaseComponentArray>> REComponentArrays{};
+
+};
