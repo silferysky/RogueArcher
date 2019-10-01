df0dce6b710089672ddb319015a5ebed3bb23b15
UnGlame <35512452+UnGlame@users.noreply.github.com>
2019-10-01 14:30:29 +0800

    Edited log output, CLEARSTRING moved to logger

--- Scripts/ObjectFactory.h
+++ Scripts/ObjectFactory.h
@@ -17,7 +17,6 @@ private:
 };
 
 //MACROS FOR OBJECT FACTORY
-#define CLEARSTRING(s) s.clear(); s.str("")
 #define SETSTRING(str, i1, cmp, i2) str << "e" << i1 << cmp << i2
 #define SETSSTOSTR(ss) stdstr = ss.str(); cstr = stdstr.c_str()
 #define CLEARNSETSTR(s, i1, cmp, i2) s.clear(); s.str(""); SETSTRING(s, i1, cmp, i2); SETSSTOSTR(s)
