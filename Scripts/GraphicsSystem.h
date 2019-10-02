7ab4b7fbe5be1353093c5522ca4b01ab9ba575c9
Javier <javier_foo@hotmail.com>
2019-10-02 16:29:14 +0800

    Updated movement for physics

--- Scripts/GraphicsSystem.h
+++ Scripts/GraphicsSystem.h
@@ -10,6 +10,24 @@
 class Timer;
 class GraphicsSystem : public System, public EventListener
 {
+	float quadVertices[40] =
+	{
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f, 1.0f,    1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, 1.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f, 1.0f,    0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f, 1.0f,   0.0f, 1.0f   // top left 
+	};
+
+	static constexpr unsigned int quadIndices[6] =
+	{
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	GLuint m_VAO;
+	GLuint m_VBO;
+	GLuint m_EBO;
 public:
 	GraphicsSystem() = default;
 	~GraphicsSystem() = default;
--- Scripts/SpriteComponent.cpp
+++ Scripts/SpriteComponent.cpp
@@ -14,31 +14,6 @@ SpriteComponent::SpriteComponent() : m_effectMat{ 1.0 }
 	std::string fragmentShader = BasicIO::ReadFile("fragmentShader.txt");
 
 	m_shader = CreateShader(vertexShader, fragmentShader);
-
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-	glGenVertexArrays(1, &m_VAO);
-	glBindVertexArray(m_VAO);
-	
-	glGenBuffers(1, &m_VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
-
-	glGenBuffers(1, &m_EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices), quadIndices, GL_STATIC_DRAW);
-	
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	// color attribute
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	// texture coord attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(7 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-	//glBindBuffer(GL_ARRAY_BUFFER, 0); //Reset
-	glBindVertexArray(0); //Reset
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
 void SpriteComponent::setTexture(const char* texture)
@@ -48,15 +23,6 @@ void SpriteComponent::setTexture(const char* texture)
 	glBindTexture(GL_TEXTURE_2D, m_texture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-}
-
-void SpriteComponent::setShader(std::string vShader, std::string fShader)
-{
-	std::string vertexShader = BasicIO::ReadFile(vShader);
-
-	std::string fragmentShader = BasicIO::ReadFile(fShader);
-
-	m_shader = CreateShader(vertexShader, fragmentShader);
 }
 
 void SpriteComponent::draw(TransformComponent* transform)
@@ -70,7 +36,6 @@ void SpriteComponent::draw(TransformComponent* transform)
 		//draw
 		 // Use the shader program for drawing
 		// model to world, world to view, view to projection
-
 
 		//offset by translation of camera, inverse of rotation
 
@@ -87,17 +52,8 @@ void SpriteComponent::draw(TransformComponent* transform)
 		GLint transformLocation = glGetUniformLocation(m_shader, "transform");
 		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transformMat));
 
-		glBindVertexArray(m_VAO);
 		// Draw the Mesh
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		// Unbind VAO after drawing
-
-		transformMat = glm::translate(transformMat, { transform->getPosition().x * 3, transform->getPosition().y, 1.0f });
-		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transformMat));
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-
-		glBindVertexArray(0);
 	}
 }
 
@@ -109,19 +65,4 @@ GLuint SpriteComponent::getTexture() const
 GLuint SpriteComponent::getShader() const
 {
 	return m_shader;
-}
-
-GLuint SpriteComponent::getVAO() const
-{
-	return m_VAO;
-}
-
-GLuint SpriteComponent::getVBO() const
-{
-	return m_VBO;
-}
-
-GLuint SpriteComponent::getEBO() const
-{
-	return m_EBO;
 }
