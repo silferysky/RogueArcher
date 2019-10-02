e098b32349cdbddec02d8ab3ca2de61941581c72
UnGlame <35512452+UnGlame@users.noreply.github.com>
2019-10-02 23:14:24 +0800

    Added math to smoke Elie

--- RogueEngine/Source/CollisionManager.h
+++ RogueEngine/Source/CollisionManager.h
@@ -12,6 +12,30 @@ public:
 	CollisionManager() = default;
 	~CollisionManager() = default;
 
+	// BOUNDING CIRCLE
+	int DynamicCircleVsLineSegment(const REMath::Circle& circle,				//Circle data - input 
+		const Vec2& ptEnd,			//End circle position - input
+		const REMath::LineSegment& lineSeg,			//Line segment - input 
+		Vec2& interPt,				//Intersection position of the circle - output 
+		Vec2& normalAtCollision,		//Normal vector at collision time - output
+		float& interTime,					//Intersection time ti - output
+		bool& checkLineEdges);
+	int DynamicCircleVsLineEdge(bool withinBothLines,
+		const REMath::Circle& circle,
+		const Vec2& ptEnd,
+		const REMath::LineSegment& lineSeg,
+		Vec2& interPt,
+		Vec2& normalAtCollision,
+		float& interTime);
+	int CollisionIntersection_RayCircle(const REMath::Ray& ray, const REMath::Circle& circle, float& interTime);
+	int CollisionIntersection_CircleCircle(const REMath::Circle& circleA,
+		const Vec2& velA,
+		const REMath::Circle& circleB,
+		const Vec2& velB,
+		Vec2& interPtA,
+		Vec2& interPtB,
+		float& interTime);
+
 	// AXIS-ALIGNED BOUNDING BOX
 	void updateAABB(AABB& collider, const TransformComponent& transform);
 	bool staticAABBvsAABB(const AABB& aabb1, const AABB& aabb2);
