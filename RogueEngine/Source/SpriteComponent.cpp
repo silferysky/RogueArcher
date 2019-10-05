e4f30543e2246a4e24d1caab63380c5e8d0c6741
Javier <javier_foo@hotmail.com>
2019-10-05 22:20:34 +0800

    Reorganised draw function

--- RogueEngine/Source/SpriteComponent.cpp
+++ RogueEngine/Source/SpriteComponent.cpp
@@ -1,12 +1,10 @@
 #include "SpriteComponent.h"
 #include "REEngine.h"
 
-SpriteComponent::SpriteComponent() : m_effectMat{ 1.0 }
+SpriteComponent::SpriteComponent()
 {
 	// create this once
 	m_texture = gEngine.m_coordinator.loadTexture("CharaTest.bmp");
-
-	m_shader = Shader("vertexShader.txt", "fragmentShader.txt");
 }
 
 void SpriteComponent::setTexture(const char* texture)
@@ -14,50 +12,7 @@ void SpriteComponent::setTexture(const char* texture)
 	m_texture = gEngine.m_coordinator.loadTexture(texture);
 }
 
-void SpriteComponent::setShader(std::string vShader, std::string fShader)
-{
-	m_shader = Shader(vShader, fShader);
-}
-
-void SpriteComponent::draw(TransformComponent* transform)
-{
-	auto transformMat = glm::mat4(1.0f);
-
-	transformMat = glm::translate(transformMat, { transform->getPosition().x, transform->getPosition().y, 1.0f});
-	transformMat = glm::scale(transformMat, glm::vec3(transform->getScale().x, transform->getScale().y, 1.0f));
-	transformMat = glm::rotate(transformMat, transform->getRotation(), glm::vec3(0.0f, 0.0f, 1.0f));
-		
-	glBindTexture(GL_TEXTURE_2D, m_texture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	//draw
-	// Use the shader program for drawing
-	// model to world, world to view, view to projection
-
-	//offset by translation of camera, inverse of rotation
-
-	glUseProgram(m_shader.GetShader());
-
-	m_effectMat = projMat * transformMat;
-
-	GLint transformLocation = glGetUniformLocation(m_shader.GetShader(), "transform");
-	glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(m_effectMat));
-
-	// Draw the Mesh
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0); //Reset
-	glBindVertexArray(0); //Reset
-	glUseProgram(0);
-}
-
 GLuint SpriteComponent::getTexture() const
 {
 	return m_texture;
-}
-
-GLuint SpriteComponent::getShader() const
-{
-	return m_shader.GetShader();
 }
--- RogueEngine/Source/SpriteComponent.h
+++ RogueEngine/Source/SpriteComponent.h
@@ -13,17 +13,10 @@
 class SpriteComponent
 {
 	GLuint m_texture;
-	Shader m_shader;
 public:
-	glm::mat4 m_effectMat;
 	SpriteComponent();
 	~SpriteComponent() = default;
 
 	void setTexture(const char* texture);
-	void setShader(std::string vShader, std::string fShader);
-
-	void draw(TransformComponent* transform);
-
 	GLuint getTexture() const;
-	GLuint getShader() const;
 };
