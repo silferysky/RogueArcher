ca1e2630969a4123d7443adebc0c558369623cbc
Terence <deathysayshi@gmail.com>
2019-10-09 18:28:32 +0800

    Updated ObjectFactory to use braces

--- RogueEngine/Source/ObjectFactory.h
+++ RogueEngine/Source/ObjectFactory.h
@@ -18,6 +18,7 @@ private:
 //MACROS FOR OBJECT FACTORY
 #define SETSTRING(str, i1, cmp) str << "Entity" << i1 << cmp
 #define SETSSTOSTR(ss) stdstr = ss.str(); cstr = stdstr.c_str()
-#define CLEARNSETSTR(s, i1, cmp) s.clear(); s.str(""); SETSTRING(s, i1, cmp); SETSSTOSTR(s)
+#define CLEARSTR(s) s.clear(); s.str("")
+#define CLEARNSETSTR(s, i1, cmp) CLEARSTR(s); SETSTRING(s, i1, cmp); SETSSTOSTR(s)
 #define MAX_SAVE_ENTITY 8
 #define MIN_SAVE_ENTITY 2
