4c44922c4984cdff016cb17ccf40f667d5615c10
Terence Chan <deathysayshi@gmail.com>
2019-10-14 16:35:52 +0800

    Added initialization for BaseAI

--- RogueEngine/Source/LogicComponent.h
+++ RogueEngine/Source/LogicComponent.h
@@ -19,6 +19,7 @@ public:
 	void	SetActiveStateBit(size_t pos);
 	bool	GetActiveStateBit(size_t pos) const;
 	void	AddAIState(AIState newState);
+	void	AddAIStateInactive(AIState newState);
 
 	std::vector<AIState> AllAIStates() const;
 
@@ -30,7 +31,7 @@ public:
 private:
 
 	enum AIType m_AIType;
-	//AllStates must be a vector since it does not necessarily act in 
+	//AllStates must be a vector since it does not necessarily act in order
 	std::vector<AIState> m_allStates;
 	//Multiple states can be active, but the first active state will be executed
 	std::bitset<static_cast<size_t>(AIState::AIState_Last)> m_activeStates;
