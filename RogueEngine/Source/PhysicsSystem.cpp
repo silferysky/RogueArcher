918f9e03dc35cdd5ffbc99ef622ce3b4780d3943
UnGlame <35512452+UnGlame@users.noreply.github.com>
2019-10-15 20:01:39 +0800

    Changing game loop

--- RogueEngine/Source/PhysicsSystem.cpp
+++ RogueEngine/Source/PhysicsSystem.cpp
@@ -11,7 +11,7 @@
 #include "Logger.h"
 
 //-------------------------------------------------------//
-//              PRIVATE MEMBER FUNCTIONS				 //
+//              PRIVATE MEMBER FUNCTIONS								 //
 //-------------------------------------------------------//
 void PhysicsSystem::applyForces(RigidbodyComponent& rigidbody) // F = ma
 {
@@ -28,9 +28,9 @@ PhysicsSystem::PhysicsSystem(Vec2 gravity)
 
 void PhysicsSystem::integrateAcceleration(RigidbodyComponent& rigidbody, TransformComponent& transform)
 {
-	transform.offSetPosition(rigidbody.getVelocity() * gDeltaTime);
+	transform.offSetPosition(rigidbody.getVelocity() * gFixedDeltaTime);
 
-	Vec2 vel = rigidbody.getAcceleration() * gDeltaTime;
+	Vec2 vel = rigidbody.getAcceleration() * gFixedDeltaTime;
 	rigidbody.offSetVelocity(vel);
 	rigidbody.setVelocity(rigidbody.getVelocity() * rigidbody.getDamping());
 
@@ -64,61 +64,65 @@ void PhysicsSystem::update()
 	Timer TimerSystem;
 	TimerSystem.TimerInit("Physics System");
 
-	// For all entities
-	std::set<Entity>::iterator iEntity;
-	for (iEntity = m_entities.begin(); iEntity != m_entities.end(); ++iEntity)
+	for (int step = 0; step < gEngine.GetStepCount(); ++step)
 	{
-		auto& rigidbody = gEngine.m_coordinator.GetComponent<RigidbodyComponent>(*iEntity);
-
-		// Skip any static rigidbodies.
-		if (rigidbody.getIsStatic())
-			continue;
-
-//		std::cout << "Entity " << *iEntity << std::endl;
-		auto& transform = gEngine.m_coordinator.GetComponent<TransformComponent>(*iEntity);
-		auto& currBoxCollider = gEngine.m_coordinator.GetComponent<BoxCollider2DComponent>(*iEntity);
-	//	auto& circleCollider = gEngine.m_coordinator.GetComponent<CircleCollider2DComponent>(*iEntity);
-
-		// Apply accForce (Forces are added if necessary)
-		applyForces(rigidbody);
-
-		// Update positions
-		integrateAcceleration(rigidbody, transform);
-
-		// Update collidables
-		m_colliderManager.updateAABB(currBoxCollider.AABB(), transform);
-		m_colliderManager.updateOBB(currBoxCollider.OBB(), transform);
-
-		// Conduct spatial partitioning
-		
-		// Test AABB/OBB Collision
-		std::set<Entity>::iterator iNextEntity = iEntity;
-
-		for (iNextEntity++; iNextEntity != m_entities.end(); ++iNextEntity)
+		// For all entities
+		std::set<Entity>::iterator iEntity;
+		for (iEntity = m_entities.begin(); iEntity != m_entities.end(); ++iEntity)
 		{
-			auto& nextBoxCollider = gEngine.m_coordinator.GetComponent<BoxCollider2DComponent>(*iNextEntity);
-			if (m_colliderManager.staticAABBvsAABB(currBoxCollider.AABB(), nextBoxCollider.AABB()))
+			auto& rigidbody = gEngine.m_coordinator.GetComponent<RigidbodyComponent>(*iEntity);
+
+			// Skip any static rigidbodies.
+			if (rigidbody.getIsStatic())
+				continue;
+
+			//		std::cout << "Entity " << *iEntity << std::endl;
+			auto& transform = gEngine.m_coordinator.GetComponent<TransformComponent>(*iEntity);
+			auto& currBoxCollider = gEngine.m_coordinator.GetComponent<BoxCollider2DComponent>(*iEntity);
+			//	auto& circleCollider = gEngine.m_coordinator.GetComponent<CircleCollider2DComponent>(*iEntity);
+
+				// Apply accForce (Forces are added if necessary)
+			applyForces(rigidbody);
+
+			// Update positions
+			integrateAcceleration(rigidbody, transform);
+
+			// Update collidables
+			m_colliderManager.updateAABB(currBoxCollider.AABB(), transform);
+			m_colliderManager.updateOBB(currBoxCollider.OBB(), transform);
+
+			// Conduct spatial partitioning
+
+			// Test AABB/OBB Collision
+			std::set<Entity>::iterator iNextEntity = iEntity;
+
+			for (iNextEntity++; iNextEntity != m_entities.end(); ++iNextEntity)
 			{
-				if(checkAABB)
-					std::cout << "Entity " << *iEntity << " AABB collides with Entity " << *iNextEntity << " AABB" << std::endl;
-				checkAABB = false;
+				auto& nextBoxCollider = gEngine.m_coordinator.GetComponent<BoxCollider2DComponent>(*iNextEntity);
+				if (m_colliderManager.staticAABBvsAABB(currBoxCollider.AABB(), nextBoxCollider.AABB()))
+				{
+					if (checkAABB)
+						std::cout << "Entity " << *iEntity << " AABB collides with Entity " << *iNextEntity << " AABB" << std::endl;
+					checkAABB = false;
+				}
+				if (m_colliderManager.staticOBBvsOBB(currBoxCollider.OBB(), nextBoxCollider.OBB()))
+				{
+					if (checkOBB)
+						std::cout << "Entity " << *iEntity << " OBB collides with Entity " << *iNextEntity << "OBB" << std::endl;
+					checkOBB = false;
+				}
 			}
-			if (m_colliderManager.staticOBBvsOBB(currBoxCollider.OBB(), nextBoxCollider.OBB()))
-			{
-				if(checkOBB)
-					std::cout << "Entity " << *iEntity << " OBB collides with Entity " << *iNextEntity << "OBB" << std::endl;
-				checkOBB = false;
-			}
+
+			// Collision Response (Contact, forces, etc)
+			// Rest, Impulse, Torque
+
+			// Reset accForce
+			rigidbody.setAccForce(Vec2());
+
+			//	std::cout << "Entity " << iEntity << "'s pos: " << transform.getPosition() << std::endl;
 		}
-
-		// Collision Response (Contact, forces, etc)
-		// Rest, Impulse, Torque
-
-		// Reset accForce
-		rigidbody.setAccForce(Vec2());
-		
-	//	std::cout << "Entity " << iEntity << "'s pos: " << transform.getPosition() << std::endl;
 	}
+
 	TimerSystem.TimerEnd("Physics System");
 }
 
@@ -229,21 +233,21 @@ void PhysicsSystem::receive(Event* ev)
 		else if (EvPressKey->GetKeyCode() == KeyPress::KeyE)
 		{
 			auto& transform = gEngine.m_coordinator.GetComponent<TransformComponent>((Entity)(m_entities.size() - 1));
-			transform.offSetScale(Vec2(3.0f, 3.0f) * gDeltaTime);
+			transform.offSetScale(Vec2(3.0f, 3.0f) * gFixedDeltaTime);
 			RE_INFO("Scaled Up!");
 		}
 
 		else if (EvPressKey->GetKeyCode() == KeyPress::KeyQ)
 		{
 			auto& transform = gEngine.m_coordinator.GetComponent<TransformComponent>((Entity)(m_entities.size() - 1));
-			transform.offSetScale(Vec2(-3.0f, -3.0f) * gDeltaTime);
+			transform.offSetScale(Vec2(-3.0f, -3.0f) * gFixedDeltaTime);
 			RE_INFO("Scaled Down!");
 		}
 
 		else if (EvPressKey->GetKeyCode() == KeyPress::KeyR)
 		{
 			auto& transform = gEngine.m_coordinator.GetComponent<TransformComponent>((Entity)(m_entities.size() - 1));
-			transform.offSetRotation(3.0f * gDeltaTime);
+			transform.offSetRotation(3.0f * gFixedDeltaTime);
 			RE_INFO("Rotated!");
 		}
 		else if (EvPressKey->GetKeyCode() == KeyPress::KeyK)
--- RogueEngine/Source/REEngine.h
+++ RogueEngine/Source/REEngine.h
@@ -3,18 +3,34 @@
 
 class REEngine
 {
+	using ChronoClock = std::chrono::time_point <std::chrono::steady_clock>;
+
+	const float MICRO_TO_SECONDS = 1000000.0f;
+	
 	float m_accumulatedTime;
 	int	m_stepCount;
+	bool m_gameIsRunning;
+	bool firstIter;
 
+	ChronoClock m_loopStart;
+	ChronoClock m_loopEnd;
+
+	void GameLoop(HDC hDC);
 	bool InitializeOpenGL();
 	void RegisterSystems();
 	void RegisterComponents();
+
 public:
+	REEngine();
 	Coordinator m_coordinator;
 
 	void init();
-	void update();
+	void update(HDC hDC);
 	void shutdown();
+
 	float GetAccumulatedTime() const;
 	int GetStepCount() const;
+
+
+	void SetGameIsRunning(bool set);
 };
