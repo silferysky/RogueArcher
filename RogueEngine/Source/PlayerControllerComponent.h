851c370b9dc5996f47101e72c8552f368fbdbbb4
Terence <deathysayshi@gmail.com>
2019-10-25 16:50:42 +0800

    Updated PlayerControllerComponent to denote which is active

--- RogueEngine/Source/PlayerControllerComponent.h
+++ RogueEngine/Source/PlayerControllerComponent.h
@@ -6,13 +6,14 @@ namespace Rogue
 	class PlayerControllerComponent
 		: public BaseComponent
 	{
+		bool m_isActive;
 	public:
 		PlayerControllerComponent() = default;
 		~PlayerControllerComponent() = default;
 
 		//No need to serialize PlayerControllerComponent as of now
-		std::string Serialize() { return std::string(); }
-		void Deserialize(std::string) {}
+		std::string Serialize();
+		void Deserialize(std::string);
 	};
 
 }
