10d9f0fb9f5e2c8af6de1cfdd6500872fc6bb9df
Terence Chan <deathysayshi@gmail.com>
2019-10-26 04:23:52 +0800

    Move logic added (Not implemented yet)

--- RogueEngine/Source/BaseAI.h
+++ RogueEngine/Source/BaseAI.h
@@ -2,6 +2,9 @@
 #include "ILogic.h"
 #include "LogicComponent.h"
 #include <memory>
+#include <vector>
+#include <queue>
+#include "Vector2D.h"
 
 namespace Rogue
 {
@@ -13,9 +16,11 @@ namespace Rogue
 		BaseAI(LogicComponent& logicComp);
 		virtual ~BaseAI() = default;
 
-		//From ILogic
+		//From ILogic (Component Related)
 		void logicInit() override;
 		void logicUpdate() override;
+
+		//From ILogic (Logic related)
 		void AIDetect() override;
 		void AIActiveStateUpdate() override;
 
@@ -23,8 +28,19 @@ namespace Rogue
 		std::shared_ptr<LogicComponent> getLogicComponent();
 		void setLogicComponent(LogicComponent& logicComp);
 
+		void AddWaypoint(Vec2 newPoint);
+		void ClearWaypoints();
+		std::vector<Vec2> GetWaypoints();
+
+		void AddNextPoint(Vec2 newPoint);
+		Vec2 GetNextPoint();
+		void PopNextPoint();
+		bool NextPointEmpty();
+
 	private:
 
 		std::shared_ptr<LogicComponent> m_logicComponent;
+		std::vector<Vec2> m_waypoints;
+		std::queue<Vec2> m_nextPoint;
 	};
 }
--- RogueEngine/Source/LogicSystem.cpp
+++ RogueEngine/Source/LogicSystem.cpp
@@ -16,10 +16,10 @@ namespace Rogue
 	LogicSystem::LogicSystem()
 		: System(SystemID::id_LOGICSYSTEM)
 	{
-		m_entityLogicMap = std::map<Entity, ILogic*>();
+		m_entityLogicMap = std::map<Entity, BaseAI*>();
 	}
 
-	void LogicSystem::AddLogicInterface(Entity entity, ILogic* logicInterface)
+	void LogicSystem::AddLogicInterface(Entity entity, BaseAI* logicInterface)
 	{
 		m_entityLogicMap.insert({ entity, logicInterface });
 	}
@@ -28,6 +28,39 @@ namespace Rogue
 	{
 		m_entityLogicMap.erase(entity);
 	}
+
+	void LogicSystem::SeekNearestWaypoint(Entity ent)
+	{
+		Vec2* currentLocation = &gEngine.m_coordinator.GetComponent<TransformComponent>(ent).getPosition();
+		Vec2* nearestWaypoint = nullptr;
+		float distance = 0.0f;
+		for (std::vector<Vec2>::iterator it = m_entityLogicMap[ent]->GetWaypoints().begin(); it != m_entityLogicMap[ent]->GetWaypoints().end(); ++it)
+		{
+			//First iteration sets first value by default
+			if (nearestWaypoint == nullptr)
+			{
+				nearestWaypoint = &*it;
+				distance = Vec2Distance(*currentLocation, *it);
+				continue;
+			}
+
+			//For all other iterations
+			float tempDistance = Vec2Distance(*currentLocation, *it);
+			if (tempDistance < distance)
+			{
+				nearestWaypoint = &*it;
+				distance = tempDistance;
+			}
+		}
+
+		//Null checker
+		if (!nearestWaypoint)
+			return;
+		
+		//Sets the best waypoint into the new location
+		m_entityLogicMap[ent]->AddNextPoint(*nearestWaypoint);
+	}
+
 
 	void LogicSystem::init()
 	{
@@ -49,6 +82,7 @@ namespace Rogue
 			//Null checker
 			if (!it->second)
 				continue;
+
 
 			//Updates the current logic. The individual AI types will handle the state on their own
 			it->second->logicUpdate();
