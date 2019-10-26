b10fe457a5eb1fcf6089a47fbf80e7c3f31aecda
UnGlame <35512452+UnGlame@users.noreply.github.com>
2019-10-26 19:37:57 +0800

    Histogram and timings working properly

--- RogueEngine/Source/Timer.h
+++ RogueEngine/Source/Timer.h
@@ -11,11 +11,14 @@ namespace Rogue
 		using ChronoTime = std::chrono::time_point <std::chrono::steady_clock>;
 		using FloatSec = std::chrono::duration<float>;
 
+		static const float s_microsecondsPerSecond;
+		static const float s_millisecondsPerSecond;
+		static const float s_microsecondsPerMillisecond;
+
 		Timer() = default;
 		void TimerInit(const char* System);
 		void TimerEnd(const char* System);
 		const std::map<const char*, float>& GetSystemTimes() const;
-		static const float s_microToSeconds;
 	private:
 		ChronoClock m_timer;
 		ChronoTime m_startTimer;
