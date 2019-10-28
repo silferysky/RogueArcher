513cb30033010ee0276c251cb53b3b349a98f393
Loh Kai Yi <liverpool0_91@hotmail.com>
2019-10-29 01:24:19 +0800

    add component and delete component works except for boxcollider2d

--- RogueEngine/Source/EditorInspector.cpp
+++ RogueEngine/Source/EditorInspector.cpp
@@ -27,19 +27,21 @@ namespace Rogue
 					Vec2 Scale = g_engine.m_coordinator.GetComponent<TransformComponent>(i.m_Entity).getScale();
 					Vec2 Position = g_engine.m_coordinator.GetComponent<TransformComponent>(i.m_Entity).getPosition();
 					float Rotation = g_engine.m_coordinator.GetComponent<TransformComponent>(i.m_Entity).getRotation();
+
 					ImGui::Text("%s", i.m_objectName.c_str());
 					ImGui::Text("Scale    ");
 					ImGui::SameLine();
 					ImGui::PushItemWidth(75);
-					ImGui::DragFloat(" ", &Scale.x);
+					ImGui::DragFloat(" ", &Scale.x,1.0f,0.0f,100000.0f);
 					ImGui::SameLine(0.0f, 36.0f);
-					ImGui::DragFloat("  ", &Scale.y);
+					ImGui::DragFloat("  ", &Scale.y,1.0f,0.0f,100000.0f);
 					g_engine.m_coordinator.GetComponent<TransformComponent>(i.m_Entity).setScale(Scale);
 
 					ImGui::Text("Rotation ");
 					ImGui::SameLine();
 					ImGui::DragFloat("   ", &Rotation,0.1f,0.0f,6.28f);
 					g_engine.m_coordinator.GetComponent<TransformComponent>(i.m_Entity).setRotation(Rotation);
+
 					ImGui::Text("Translate");
 					ImGui::SameLine();
 					ImGui::PushItemWidth(75);
@@ -48,35 +50,114 @@ namespace Rogue
 					ImGui::PushItemWidth(75);
 					ImGui::DragFloat("      ", &Position.y);
 					g_engine.m_coordinator.GetComponent<TransformComponent>(i.m_Entity).setPosition(Position);
+					ImGui::PushItemWidth(50);
+
+					if (ImGui::Button("Add Component"))
+					{
+						ImGui::OpenPopup("Add Component");
+					}
+
+					if (ImGui::Button("Delete Component"))
+					{
+						ImGui::OpenPopup("Delete Component");
+					}
+
+					if (ImGui::BeginPopup("Add Component"))
+					{
+						if (ImGui::BeginMenu("Component"))
+						{
+							if (ImGui::MenuItem("Sprite Component", nullptr, false, !g_engine.m_coordinator.CheckIfComponentExists<SpriteComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.CreateComponent<SpriteComponent>(i.m_Entity);
+							}
+							
+							if (ImGui::MenuItem("Transform Component",nullptr,false, !g_engine.m_coordinator.CheckIfComponentExists<TransformComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.CreateComponent<TransformComponent>(i.m_Entity);
+							}
+
+							if (ImGui::MenuItem("Player Controller Component", nullptr, false, !g_engine.m_coordinator.CheckIfComponentExists<PlayerControllerComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.CreateComponent<PlayerControllerComponent>(i.m_Entity);
+							}
+
+							if (ImGui::MenuItem("Logic Component", nullptr, false, !g_engine.m_coordinator.CheckIfComponentExists<LogicComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.CreateComponent<LogicComponent>(i.m_Entity);
+							}
+							ImGui::EndMenu();
+						}
+						if (ImGui::BeginMenu("Colliders"))
+						{
+							if (ImGui::MenuItem("RigidBody",nullptr,false,!g_engine.m_coordinator.CheckIfComponentExists<RigidbodyComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.CreateComponent<RigidbodyComponent>(i.m_Entity);
+							}
+
+							if (ImGui::MenuItem("Circle Collider", nullptr, false, !g_engine.m_coordinator.CheckIfComponentExists<CircleCollider2DComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.CreateComponent<CircleCollider2DComponent>(i.m_Entity);
+							}
+
+							if (ImGui::MenuItem("Box Collider 2D", nullptr, false, !g_engine.m_coordinator.CheckIfComponentExists<BoxCollider2DComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.CreateComponent<BoxCollider2DComponent>(i.m_Entity);
+							}
+
+							ImGui::EndMenu();
+						}
+						ImGui::EndPopup();
+					}
+					if (ImGui::BeginPopup("Delete Component"))
+					{
+						if (ImGui::BeginMenu("Component"))
+						{
+							if (ImGui::MenuItem("Sprite Component", nullptr, false, g_engine.m_coordinator.CheckIfComponentExists<SpriteComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.RemoveComponent<SpriteComponent>(i.m_Entity);
+							}
+
+							if (ImGui::MenuItem("Transform Component", nullptr, false, g_engine.m_coordinator.CheckIfComponentExists<TransformComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.RemoveComponent<TransformComponent>(i.m_Entity);
+							}
+
+							if (ImGui::MenuItem("Player Controller Component", nullptr, false, g_engine.m_coordinator.CheckIfComponentExists<PlayerControllerComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.RemoveComponent<PlayerControllerComponent>(i.m_Entity);
+							}
+
+							if (ImGui::MenuItem("Logic Component", nullptr, false, g_engine.m_coordinator.CheckIfComponentExists<LogicComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.RemoveComponent<LogicComponent>(i.m_Entity);
+							}
+							ImGui::EndMenu();
+						}
+						if (ImGui::BeginMenu("Colliders"))
+						{
+							if (ImGui::MenuItem("RigidBody", nullptr, false, g_engine.m_coordinator.CheckIfComponentExists<RigidbodyComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.RemoveComponent<RigidbodyComponent>(i.m_Entity);
+							}
+
+							if (ImGui::MenuItem("Circle Collider", nullptr, false, g_engine.m_coordinator.CheckIfComponentExists<CircleCollider2DComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.RemoveComponent<CircleCollider2DComponent>(i.m_Entity);
+							}
+
+							if (ImGui::MenuItem("Box Collider 2D", nullptr, false, g_engine.m_coordinator.CheckIfComponentExists<BoxCollider2DComponent>(i.m_Entity)))
+							{
+								g_engine.m_coordinator.RemoveComponent<BoxCollider2DComponent>(i.m_Entity);
+							}
+
+							ImGui::EndMenu();
+						}
+						ImGui::EndPopup();
+					}
 				}
 			}			
 
-			ImGui::PushItemWidth(50);
-			if (ImGui::Button("Add Component"))
-			{
-				ImGui::OpenPopup("Component");
-			}
-			if (ImGui::BeginPopup("Component"))
-			{
-				if (ImGui::Selectable("Sprite Component"))
-				{
-
-				}
-				if (ImGui::Selectable("Transform Component"))
-				{
-
-				}
-				if (ImGui::Selectable("Player Controller Component"))
-				{
-
-				}
-				if (ImGui::Selectable("Logic Component"))
-				{
-
-				}
-				ImGui::EndPopup();
-			}
-
+			
 		}
 
 		ImGui::End();
