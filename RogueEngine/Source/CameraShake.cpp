fd5096b3f0bc6b5165f7cf958a189a9913add195
Javier <javier_foo@hotmail.com>
2019-10-30 04:09:39 +0800

    Perlin noise

--- RogueEngine/Source/CameraShake.cpp
+++ RogueEngine/Source/CameraShake.cpp
@@ -15,7 +15,7 @@ namespace Rogue
 			m_duration = 0;
 
 			m_currentShakeMagnitude *= 0.9f; //diminish radius each frame
-			m_currentShakeAngle += (180 + (-(m_shakeAngleMagnitude / 2) + rand() % m_shakeAngleMagnitude)); //pick new angle 
+			m_currentShakeAngle += (180 + (-(m_shakeAngleMagnitude / 2) + /* int(m_pn.noise(0.45, 0.8, 0.55)) */ rand() % m_shakeAngleMagnitude)); //pick new angle 
 
 			m_shakeOffset.x = static_cast<float>(sin(m_currentShakeAngle) * m_currentShakeMagnitude);
 			m_shakeOffset.y = static_cast<float>(cos(m_currentShakeAngle) * m_currentShakeMagnitude);
