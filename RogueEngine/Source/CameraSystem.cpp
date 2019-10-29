cf03efa2ee5e22036e0b8d773230872f91c3642b
Javier <javier_foo@hotmail.com>
2019-10-29 23:59:44 +0800

    World camera toggle

--- RogueEngine/Source/CameraSystem.cpp
+++ RogueEngine/Source/CameraSystem.cpp
@@ -10,7 +10,10 @@ namespace Rogue
 	//-------------------------------------------------------//
 
 	CameraSystem::CameraSystem()
-		: System(SystemID::id_CAMERASYSTEM) {}
+		: System(SystemID::id_CAMERASYSTEM), m_worldCamera{ false },
+		m_cameraPos{ 0.0f, 0.0f, 0.0f }, m_worldUp{ 0.0f, 1.0f, 0.0f },
+		m_cameraFront{ 0.0f, 0.0f, -1.0f }, m_cameraShake{}
+	{}
 
 	void CameraSystem::Init()
 	{
@@ -23,19 +26,22 @@ namespace Rogue
 		// Set system signature.
 		g_engine.m_coordinator.SetSystemSignature<CameraSystem>(signature);
 
-		m_cameraPos = glm::vec3(0.0f, 0.0f, 0.0f);
-		m_worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
-		m_cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-
 		m_cameraRight = glm::normalize(glm::cross(m_cameraFront, m_worldUp));
 		m_cameraUp = glm::normalize(glm::cross(m_cameraRight, m_cameraFront));
-
-		m_cameraShake = CameraShake();
 	}
 
 	glm::mat4 CameraSystem::GetViewMatrix()
 	{
 		return glm::lookAt(m_cameraPos, m_cameraPos + m_cameraFront, m_cameraUp);
+	}
+
+	void CameraSystem::ToggleWorldCamera()
+	{
+		m_worldCamera = !m_worldCamera;
+
+		// Reset camera position to center
+		if (m_worldCamera)
+			m_cameraPos = { 0.0f, 0.0f, 0.0f };
 	}
 
 	void CameraSystem::Update()
@@ -45,12 +51,15 @@ namespace Rogue
 		m_cameraShake.Update(g_deltaTime);
 		auto shakeOffset = m_cameraShake.getOffset();
 		
-		// For all entities
-		for (auto entity : m_entities)
+		if (!m_worldCamera)
 		{
-			auto transformPos = g_engine.m_coordinator.GetComponent<TransformComponent>(entity).getPosition();
+			// For all entities
+			for (auto entity : m_entities)
+			{
+				auto transformPos = g_engine.m_coordinator.GetComponent<TransformComponent>(entity).getPosition();
 
-			m_cameraPos = glm::vec3(transformPos.x + shakeOffset.x, transformPos.y + shakeOffset.y, 0.0f);
+				m_cameraPos = glm::vec3(transformPos.x + shakeOffset.x, transformPos.y + shakeOffset.y, 0.0f);
+			}
 		}
 
 		g_engine.m_coordinator.EndTimeSystem("Camera System");
@@ -68,6 +77,9 @@ namespace Rogue
 
 			if (keycode == KeyPress::KeyP)
 				m_cameraShake.SetShake(13.0f);
+
+			if (keycode == KeyPress::KeyShift)
+				ToggleWorldCamera();
 
 			return;
 		} //End KeyTriggered
