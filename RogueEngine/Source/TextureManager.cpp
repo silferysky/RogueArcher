58211bcf22fde6959ca6ce2e592be042f022fa84
Javier <javier_foo@hotmail.com>
2019-10-29 18:18:11 +0800

    further optimization

--- RogueEngine/Source/TextureManager.cpp
+++ RogueEngine/Source/TextureManager.cpp
@@ -40,10 +40,13 @@ namespace Rogue
 			);
 
 			glBindTexture(GL_TEXTURE_2D, newTexture.m_texture);
+			
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+			glGenerateMipmap(GL_TEXTURE_2D);
 
 			if (newTexture.m_data == NULL)
 				RE_INFO("STBI error: ", stbi_failure_reason());
