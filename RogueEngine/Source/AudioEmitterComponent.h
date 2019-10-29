0f24e7272e5d898fa0e27b99cc9b423e497cae2e
Javier <javier_foo@hotmail.com>
2019-10-30 01:19:45 +0800

    fixed

--- RogueEngine/Source/AudioEmitterComponent.h
+++ RogueEngine/Source/AudioEmitterComponent.h
@@ -6,8 +6,12 @@ namespace Rogue
 	class AudioEmitterComponent
 		: public BaseComponent
 	{
+		std::string m_id;
 	public:
 		AudioEmitterComponent() = default;
+
+		void setID(const std::string& id);
+		std::string getID() const;
 
 		std::string Serialize();
 		void Deserialize(std::string toDeserialize);
