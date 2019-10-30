bf46328671886302f7a38e29b0d7baa8216493c6
Terence <deathysayshi@gmail.com>
2019-10-30 12:31:07 +0800

    JUMP

--- RogueEngine/Source/PlayerControllerSystem.cpp
+++ RogueEngine/Source/PlayerControllerSystem.cpp
@@ -56,6 +56,19 @@ namespace Rogue
 				g_engine.SetTimeScale(0.1f);
 			}
 
+			if (keycode == KeyPress::KeySpace)
+			{
+				for (std::set<Entity>::iterator iEntity = m_entities.begin(); iEntity != m_entities.end(); ++iEntity)
+				{
+					//For 1st entity
+					if (iEntity == m_entities.begin() && g_engine.m_coordinator.CheckIfComponentExists<RigidbodyComponent>(*iEntity))
+					{
+						auto& rigidbody = g_engine.m_coordinator.GetComponent<RigidbodyComponent>(*iEntity);
+						rigidbody.addForce(Vec2(0.0f, 50000.0f));
+					}
+				}
+			}
+
 			return;
 		} //End KeyTriggered
 		case EventType::EvKeyPressed:
