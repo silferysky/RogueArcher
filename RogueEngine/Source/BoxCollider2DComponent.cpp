a2e2d3dfadc85cb181af8da6af69f3e725124b42
UnGlame <35512452+UnGlame@users.noreply.github.com>
2019-11-01 02:04:12 +0800

    Collider offsets serialization

--- RogueEngine/Source/BoxCollider2DComponent.cpp
+++ RogueEngine/Source/BoxCollider2DComponent.cpp
@@ -52,6 +52,12 @@ namespace Rogue
 			ss << m_obb.modelVerts()[i].x << ";" << m_obb.modelVerts()[i].y << ";";
 		}
 
+		ss << m_aabb.getCenterOffSet().x << ";";
+		ss << m_aabb.getCenterOffSet().y << ";";
+
+		ss << m_aabb.getScaleOffSet().x << ";";
+		ss << m_aabb.getScaleOffSet().y;
+
 		return ss.str();
 	}
 
@@ -60,8 +66,9 @@ namespace Rogue
 		std::istringstream ss(toDeserialize);
 		std::string s1, s2;		//s2 is used if two are needed
 		std::vector<Vec2> vertexList{};
-		int size = 0;
+		size_t size = 0;
 
+		// For OBB
 		if (std::getline(ss, s1, ';'))
 		{
 			size = static_cast<size_t>(stoi(s1));
@@ -76,6 +83,26 @@ namespace Rogue
 
 		m_obb.setModelVerts(vertexList);
 		m_obb.setSize(size);
+
+		// For AABB
+		float x = 0.0f;
+		float y = 0.0f;
+
+		if(std::getline(ss, s1, ';'))
+			x = std::stof(s1);
+
+		if (std::getline(ss, s1, ';'))
+			y = std::stof(s1);
+
+		m_aabb.setCenterOffSet(Vec2{ x, y });
+
+		if (std::getline(ss, s1, ';'))
+			x = std::stof(s1);
+
+		if (std::getline(ss, s1, ';'))
+			y = std::stof(s1);
+
+		m_aabb.setScaleOffSet(Vec2{ x, y });
 	}
 
 
--- RogueEngine/Source/CircleCollider2DComponent.cpp
+++ RogueEngine/Source/CircleCollider2DComponent.cpp
@@ -8,7 +8,12 @@ namespace Rogue
 	{
 		//Radius
 		std::ostringstream ss;
-		ss << m_collider.getRadius();
+		ss << m_collider.getRadius() << ";";
+
+		ss << m_collider.getCenterOffSet().x << ";";
+		ss << m_collider.getCenterOffSet().y << ";";
+		ss << m_collider.getScaleOffSet().x << ";";
+		ss << m_collider.getScaleOffSet().y;
 
 		return ss.str();
 	}
@@ -20,9 +25,27 @@ namespace Rogue
 		//int counter = 0;		//Needed to take in for multiple values
 		//int sets = 2;			//Sets represents the number of "sets" that must be taken in simultaneously. Aka vec2 or more than 1	parameter to set
 
-		while (std::getline(ss, s1, ';'))
-		{
+		if (std::getline(ss, s1, ';'))
 			m_collider.setRadius(std::stof(s1));
-		}
+
+		// For Offsets
+		float x = 0.0f;
+		float y = 0.0f;
+
+		if (std::getline(ss, s1, ';'))
+			x = stof(s1);
+
+		if (std::getline(ss, s1, ';'))
+			y = stof(s1);
+
+		m_collider.setCenterOffSet(Vec2{ x, y });
+
+		if (std::getline(ss, s1, ';'))
+			x = stof(s1);
+
+		if (std::getline(ss, s1, ';'))
+			y = stof(s1);
+
+		m_collider.setScaleOffSet(Vec2{ x, y });
 	}
 }
