f73c06c6529c27281a73bebb8b39c69ba4848a69
Javier <javier_foo@hotmail.com>
2019-11-03 07:39:26 +0800

    Organised shaders

--- RogueEngine/Source/ShaderManager.h
+++ RogueEngine/Source/ShaderManager.h
@@ -10,16 +10,17 @@ namespace Rogue
 	class ShaderManager
 	{
 		std::map<std::string, Shader> ShaderMap;
+		std::string directory = "Resources/Shaders/";
 	public:
 		ShaderManager() = default;
 		~ShaderManager() = default;
 
 		void Init()
 		{
-			ShaderMap.emplace("Object Shader", Shader("vertexShader.txt", "fragmentShader.txt"));
-			ShaderMap.emplace("Screen Shader", Shader("vertexScreenShader.txt", "fragmentScreenShader.txt"));
-			ShaderMap.emplace("Debug Shader", Shader("vertexLineShader.txt", "fragmentLineShader.txt"));
-			ShaderMap.emplace("Font Shader", Shader("vertexFontShader.txt", "fragmentFontShader.txt"));
+			ShaderMap.emplace("Object Shader", Shader(directory + "vertexShader.txt", directory + "fragmentShader.txt"));
+			ShaderMap.emplace("Screen Shader", Shader(directory + "vertexScreenShader.txt", directory + "fragmentScreenShader.txt"));
+			ShaderMap.emplace("Debug Shader", Shader(directory + "vertexLineShader.txt", directory + "fragmentLineShader.txt"));
+			ShaderMap.emplace("Font Shader", Shader(directory + "vertexFontShader.txt", directory + "fragmentFontShader.txt"));
 		}
 
 		const std::map<std::string, Shader>& getShaderMap() const
