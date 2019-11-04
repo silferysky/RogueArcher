7803d1f3062de28cecb760e12bed40efc10fc1d3
Loh Kai Yi <liverpool0_91@hotmail.com>
2019-11-05 04:13:57 +0800

    added level 4 and 5

--- RogueEngine/Source/EditorProject.cpp
+++ RogueEngine/Source/EditorProject.cpp
@@ -35,5 +35,33 @@ namespace Rogue
 	void ImGuiProject::Shutdown()
 	{
 	}
+
+	void ImGuiProject::DisplayDirectoryTreeImp(const std::experimental::filesystem::path& pathToShow, int level)
+	{
+		if (std::experimental::filesystem::exists(pathToShow) && std::experimental::filesystem::is_directory(pathToShow))
+		{
+			auto lead = std::string(level * 3, ' ');
+			for (const auto& entry : std::experimental::filesystem::directory_iterator(pathToShow))
+			{
+				auto filename = entry.path().filename();
+				if (std::experimental::filesystem::is_directory(entry.status()))
+				{
+					std::cout << lead << "[+] " << filename << "\n";
+					DisplayDirectoryTreeImp(entry, level + 1);
+					std::cout << "\n";
+				}
+				else if (std::experimental::filesystem::is_regular_file(entry.status()))
+				{
+					//DisplayFileInfo(entry, lead, filename);
+				}
+				else
+					std::cout << lead << " [?]" << filename << "\n";
+			}
+		}
+	}
+	void ImGuiProject::DisplayDirectoryTree(const std::experimental::filesystem::path& pathToShow)
+	{
+		DisplayDirectoryTreeImp(pathToShow, 0);
+	}
 }
 
--- RogueEngine/Source/EditorProject.h
+++ RogueEngine/Source/EditorProject.h
@@ -4,9 +4,10 @@
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_win32.h"
 #include "glew.h"
+#include <filesystem>
+#include <iostream>
 
 namespace Rogue
-
 {
 	class ImGuiProject : public IEditable
 	{
@@ -16,5 +17,15 @@ namespace Rogue
 		virtual void Init() override;
 		virtual void Update() override;
 		virtual void Shutdown() override;
+		
+		void DisplayDirectoryTreeImp(const  std::experimental::filesystem::path& pathToShow, int level);
+		void DisplayDirectoryTree(const std::experimental::filesystem::path& pathToShow);
+		struct Data
+		{
+			std::string m_filePath;
+			std::string m_name;
+			std::vector<std::string> m_data;
+		};
+		std::vector<Data> m_Directories;
 	};
 }
