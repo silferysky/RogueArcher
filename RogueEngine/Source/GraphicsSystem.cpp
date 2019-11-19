a8bb0bb867ee0b434e05fbc2bfd3ca656956f099
Javier <javier_foo@hotmail.com>
2019-11-20 03:40:48 +0800

    Draw priority moved

--- RogueEngine/Source/GraphicsSystem.cpp
+++ RogueEngine/Source/GraphicsSystem.cpp
@@ -66,7 +66,7 @@ namespace Rogue
 		// For all entities
 		for (auto entity : m_entities)
 		{
-			auto priority = g_engine.m_coordinator.GetComponent<SpriteComponent>(entity).getDrawPriority();
+			auto priority = g_engine.m_coordinator.GetComponent<TransformComponent>(entity).GetZ();
 
 			m_drawQueue.insert(std::make_pair(priority, entity));
 		}
