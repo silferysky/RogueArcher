d7d4cbf11408d88b8dbffad68620a4727844f30c
Terence <deathysayshi@gmail.com>
2019-11-23 03:15:42 +0800

    Update EditorHierarchyInfo.h

--- RogueEngine/Source/EditorHierarchyInfo.h
+++ RogueEngine/Source/EditorHierarchyInfo.h
@@ -12,9 +12,12 @@ namespace Rogue
 		bool m_selected = false;
 		Entity m_Entity = 0;
 
+		Entity m_parent = MAX_ENTITIES;
+		std::vector<Entity> m_children;
+
 		HierarchyInfo() = default;
 		HierarchyInfo(Entity entity, std::string_view name,std::string_view tag = "")
-			: m_Entity{ entity }, m_objectName{ name }, m_tag{ tag }{}
+			: m_Entity{ entity }, m_objectName{ name }, m_tag{ tag }, m_parent{ MAX_ENTITIES }{}
 	};
 
 	class DirectoryInfo
