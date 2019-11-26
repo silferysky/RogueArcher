001153741093782b9fb46a760dd61caac92b3bed
Javier <javier_foo@hotmail.com>
2019-11-26 13:52:08 +0800

    particle system rework + delete queue

--- RogueEngine/Source/ParticleEmitterSystem.cpp
+++ RogueEngine/Source/ParticleEmitterSystem.cpp
@@ -1,7 +1,7 @@
 #include "Precompiled.h"
 #include "ParticleEmitterSystem.h"
 #include "Main.h"
-#include "ParticleComponent.h"
+#include "ParticleEmitterComponent.h"
 
 
 namespace Rogue
@@ -31,18 +31,7 @@ namespace Rogue
 			if (!pEmitter.GetIsActive())
 				continue;
 
-			switch (pEmitter.GetParticleType())
-			{
-			case ParticleType::EXPLOSION:
-				GenerateExplosion(entity);
-				break;
-			case ParticleType::SPRAY:
-				GenerateSpray(entity);
-				break;
-			case ParticleType::TRAIL:
-				GenerateTrail(entity);
-				break;
-			}
+			GenerateParticles(entity);
 
 			if (!pEmitter.GetIsContinuous())
 				pEmitter.SetIsActive(false);
@@ -57,11 +46,14 @@ namespace Rogue
 	{
 	}
 
-	void ParticleEmitterSystem::GenerateExplosion(const Entity& entity)
+	void ParticleEmitterSystem::GenerateParticles(const Entity& entity)
 	{
 		auto& pEmitter = g_engine.m_coordinator.GetComponent<ParticleEmitterComponent>(entity);
 		auto& transform = g_engine.m_coordinator.GetComponent<TransformComponent>(entity);
 		const float& magnitude = pEmitter.GetMagnitude();
+		const Vec2& scale = pEmitter.GetScale();
+		const Vec2& velocityFactor = pEmitter.GetVelocity();
+		const Vec2& positionalOffset = pEmitter.GetPositionalOffset();
 
 		for (int i = 0; i < 100 * magnitude; ++i)
 		{
@@ -70,21 +62,20 @@ namespace Rogue
 
 			TransformComponent& ballTransform = g_engine.m_coordinator.CreateComponent<TransformComponent>(particle);
 			Vec2 pos = transform.GetPosition();
-			strstream << pos.x << ";"
-				<< pos.y << ";"
-				<< "20;20;0;100";
+
+			strstream << pos.x + RandFloat(positionalOffset.x) << ";"
+				      << pos.y + RandFloat(positionalOffset.x) << ";"
+				      << scale.x << ";"
+					  << scale.y << ";"
+				      << "0" << ";"
+				      << transform.GetZ();
 			ballTransform.Deserialize(strstream.str());
 
-			strstream.str(std::string());;
+			strstream.str(std::string()); // clear the string stream
 
 			Vec2 velocity;
-			velocity.y = RandFloat() * 10 * magnitude;
-
-			// split the directions between left and right
-			if (rand() % 2)
-				velocity.x = RandFloat() * 10 * magnitude;
-			else
-				velocity.x = -RandFloat() * 10 * magnitude;
+			velocity.x = RandFloat() * 100 * velocityFactor.x * cos(pEmitter.GetArc());
+			velocity.y = RandFloat() * 100 * velocityFactor.y * sin(pEmitter.GetArc());
 
 			RigidbodyComponent& rigidbody = g_engine.m_coordinator.CreateComponent<RigidbodyComponent>(particle);
 			rigidbody.Deserialize("0;0;0;0;1;1;0");
@@ -98,51 +89,8 @@ namespace Rogue
 			sprite.Deserialize(strstream.str());
 
 			ParticleComponent& particleComp = g_engine.m_coordinator.CreateComponent<ParticleComponent>(particle);
-			particleComp.SetLifetime(RandFloat(20.0f));
+			particleComp.SetLifetime(RandFloat(pEmitter.GetLifetimeLimit()));
 			
-			HierarchyInfo newInfo(particle, "Particle " + std::to_string(i));
-			g_engine.m_coordinator.GetActiveObjects().push_back(particle);
-			g_engine.m_coordinator.GetHierarchyInfo(particle) = newInfo;
-		}
-	}
-
-	void ParticleEmitterSystem::GenerateSpray(const Entity& entity)
-	{
-
-	}
-
-	void ParticleEmitterSystem::GenerateTrail(const Entity& entity)
-	{
-		auto& pEmitter = g_engine.m_coordinator.GetComponent<ParticleEmitterComponent>(entity);
-		auto& transform = g_engine.m_coordinator.GetComponent<TransformComponent>(entity);
-		auto& ownerRBody = g_engine.m_coordinator.GetComponent<RigidbodyComponent>(entity);
-
-		const float& magnitude = pEmitter.GetMagnitude();
-
-		for (int i = 0; i < 100 * magnitude; ++i)
-		{
-			std::ostringstream strstream;
-			Entity particle = g_engine.m_coordinator.CreateEntity();
-
-			TransformComponent& ballTransform = g_engine.m_coordinator.CreateComponent<TransformComponent>(particle);
-			Vec2 pos = transform.GetPosition();
-
-			// The RandFloat here is to slightly offset the particle
-			strstream << pos.x + RandFloat(5.0f) << ";"
-				<< pos.y + RandFloat(5.0f) << ";"
-				<< "20;20;0";
-
-			RigidbodyComponent& rigidbody = g_engine.m_coordinator.CreateComponent<RigidbodyComponent>(particle);
-			rigidbody.Deserialize("0;0;0;0;1;1;0");
-			rigidbody.addForce(ownerRBody.getVelocity()/ RandFloat()); // only a fraction of the velocity is applied
-
-			SpriteComponent& sprite = g_engine.m_coordinator.CreateComponent<SpriteComponent>(particle);
-			strstream << pEmitter.GetTexturePath().data() << ";1;1;1;1;1";
-			sprite.Deserialize(strstream.str());
-
-			ParticleComponent& particleComp = g_engine.m_coordinator.CreateComponent<ParticleComponent>(particle);
-			particleComp.SetLifetime(RandFloat(20.0f));
-
 			HierarchyInfo newInfo(particle, "Particle " + std::to_string(i));
 			g_engine.m_coordinator.GetActiveObjects().push_back(particle);
 			g_engine.m_coordinator.GetHierarchyInfo(particle) = newInfo;
