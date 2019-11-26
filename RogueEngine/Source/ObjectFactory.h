650e3df53cd6343eb4d4f60fc0049fa0d1f75cb2
Terence Chan <deathysayshi@gmail.com>
2019-11-26 12:26:01 +0800

    Proper Patricle objects deleting

--- RogueEngine/Source/ObjectFactory.h
+++ RogueEngine/Source/ObjectFactory.h
@@ -56,14 +56,14 @@ namespace Rogue
 
 	//MACROS (HIERARCHY)
 #define CREATE_HIERARCHY_OBJ_TAG(entityValue, name, tag)	HierarchyInfo newInfo(entityValue, name, tag); \
-														g_engine.m_coordinator.GetActiveObjects().push_back(entityValue); \
-														g_engine.m_coordinator.GetHierarchyInfo(entityValue) = newInfo; \
-														SceneManager::instance().IncrementObjectIterator();
-
-#define CREATE_HIERARCHY_OBJ(entityValue, name)			HierarchyInfo newInfo(entityValue, name); \
-														g_engine.m_coordinator.GetActiveObjects().push_back(entityValue); \
-														g_engine.m_coordinator.GetHierarchyInfo(entityValue) = newInfo; \
-														SceneManager::instance().IncrementObjectIterator();
+															g_engine.m_coordinator.GetActiveObjects().push_back(entityValue); \
+															g_engine.m_coordinator.GetHierarchyInfo(entityValue) = newInfo; \
+															SceneManager::instance().IncrementObjectIterator();
+	
+#define CREATE_HIERARCHY_OBJ(entityValue, name)				HierarchyInfo newInfo(entityValue, name); \
+															g_engine.m_coordinator.GetActiveObjects().push_back(entityValue); \
+															g_engine.m_coordinator.GetHierarchyInfo(entityValue) = newInfo; \
+															SceneManager::instance().IncrementObjectIterator();
 
 //cstr will go out of scope if you choose to do strstream.str().c_str()
 //This is the proper (Non macro) way of setting the string
