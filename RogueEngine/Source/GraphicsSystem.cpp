d0eef7f893790638440919a12ccc0f23a71827c3
Javier <javier_foo@hotmail.com>
2019-11-27 11:47:16 +0800

    background color

--- RogueEngine/Source/GraphicsSystem.cpp
+++ RogueEngine/Source/GraphicsSystem.cpp
@@ -74,7 +74,7 @@ namespace Rogue
 		glBindFramebuffer(GL_FRAMEBUFFER, m_FBO);
 
 		//auto msg = g_engine.GetWindowMessage();
-
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		// clear the buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
