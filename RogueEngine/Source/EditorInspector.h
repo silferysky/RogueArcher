4dc9df949d37008f3e94d50ae6458697e8645fb2
lohkaiyi <51329075+lohkaiyi@users.noreply.github.com>
2019-12-01 22:22:17 +0800

    Header file

--- RogueEngine/Source/EditorInspector.h
+++ RogueEngine/Source/EditorInspector.h
@@ -1,3 +1,23 @@
+/* Start Header ************************************************************************/
+/*!
+\file           EditorInspector.h
+\project        Exale
+\author         Loh Kai Yi,kaiyi.loh,390002918 (70%)
+				Lim Jia Hao, l.jiahao, 390006518 (10%)
+				Chan Wai Kit Terence, c.terence, 440005918 (10%)
+				Javier Foo, javier.foo, 440002318 (10%)
+\par            kaiyi.loh\@digipen.edu
+\date           1 December,2019
+\brief          This file contains the function definitions for EditorInspector
+
+All content (C) 2019 DigiPen (SINGAPORE) Corporation, all rights
+reserved.
+
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
 #pragma once
 #include "IEditable.h"
 #include "EditorHierarchyInfo.h"
--- RogueEngine/Source/PickingManager.h
+++ RogueEngine/Source/PickingManager.h
@@ -1,3 +1,20 @@
+/*Start Header************************************************************************ /
+/*!
+\file           PickingManager.h
+\project        Exale
+\author         Lim Jia Hao, l.jiahao, 390006518 (100%)
+\par            l.jiahao\@digipen.edu
+\date           1 December,2019
+\brief          This file contains the function definitions for PickingManager
+
+All content (C) 2019 DigiPen (SINGAPORE) Corporation, all rights
+reserved.
+
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
 #pragma once
 #include "Vector2D.h"
 #include "AABB.h"
