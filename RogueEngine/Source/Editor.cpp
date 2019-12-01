7282cd36e1574329d0ac02e334917d243ee201f0
lohkaiyi <51329075+lohkaiyi@users.noreply.github.com>
2019-12-01 22:22:17 +0800

    Header file

--- RogueEngine/Source/Editor.cpp
+++ RogueEngine/Source/Editor.cpp
@@ -1,3 +1,20 @@
+/* Start Header ************************************************************************/
+/*!
+\file           Editor.cpp
+\project        Exale
+\author         Loh Kai Yi,kaiyi.loh,390002918 (100%)
+\par            kaiyi.loh\@digipen.edu
+\date           1 December,2019
+\brief          This file contains the functions for editor
+
+All content (C) 2019 DigiPen (SINGAPORE) Corporation, all rights
+reserved.
+
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*/
+/* End Header **************************************************************************/
 #include "Precompiled.h"
 #include "Editor.h"
 #include "EventDispatcher.h"
