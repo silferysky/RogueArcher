45aa891579c578b2d2dae910c6d0d16d2f8a71a9
Loh Kai Yi <liverpool0_91@hotmail.com>
2020-01-06 00:41:04 +0800

    undo redo WIP

---
+++ RogueEngine/Source/ICommandable.h
@@ -0,0 +1,114 @@
+#pragma once
+namespace Rogue
+{
+	class ICommandable
+	{
+	public:
+		ICommandable() = default;
+		virtual ~ICommandable() = default;
+
+		virtual void Execute() = 0;
+		virtual void Undo() = 0;
+		virtual void Redo() = 0;
+	};
+
+	class Model
+	{
+		bool m_On;
+		int m_Count;
+	public:
+		Model() {}
+		void SwitchOn()
+		{
+			m_On = true;
+		}
+		void SwitchOff()
+		{
+			m_On = false;
+		}
+		void switchCount(int count)
+		{
+			m_Count = count;
+		}
+
+		bool isOn()
+		{
+			return m_On;
+		}
+
+		int getCount()
+		{
+			return m_Count;
+		}
+	};
+
+	class OnCommand :public ICommandable
+	{
+		Model* m_Model;
+	public:
+		OnCommand(Model& model) :m_Model(&model)
+		{
+		}
+		void Execute() override
+		{
+			m_Model->SwitchOn();
+		}
+		void Undo() override
+		{
+			m_Model->SwitchOff();
+		}
+		void Redo() override
+		{
+			m_Model->SwitchOn();
+		}
+	};
+
+	class OffCommand :public ICommandable
+	{
+		OnCommand m_OnCommand;
+	public:
+		OffCommand(Model& model) :m_OnCommand(model)
+		{
+		}
+		void Execute() override
+		{
+			m_OnCommand.Undo();
+		}
+		void Undo() override
+		{
+			m_OnCommand.Execute();
+		}
+		void Redo() override
+		{
+			m_OnCommand.Undo();
+		}
+	};
+	
+	class SwitchCommand : public ICommandable
+	{
+		Model* m_Model;
+		int m_oldCount;
+		int m_newCount;
+
+	public:
+		SwitchCommand(Model* model,int count):m_Model(model),m_newCount(count)
+		{
+		}
+
+		void Execute() override
+		{
+			m_oldCount = m_Model->getCount();
+			m_Model->switchCount(m_newCount);
+		}
+
+		void Undo() override
+		{
+			m_Model->switchCount(m_oldCount);
+		}
+
+		void Redo() override
+		{
+			m_Model->switchCount(m_newCount);
+		}
+	};
+}
