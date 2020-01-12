fa2ab40a45076ab6d29c43d21e83b7700109a3d2
Terence Chan <deathysayshi@gmail.com>
2020-01-13 03:56:16 +0800

    Updated Inspector to reflect proper values. Does not work for scale yet

--- RogueEngine/Source/EditorInspector.cpp
+++ RogueEngine/Source/EditorInspector.cpp
@@ -121,7 +121,86 @@ namespace Rogue
 						if (ImGui::CollapsingHeader("Transform"))
 						{
 							auto& trans = g_engine.m_coordinator.GetComponent<TransformComponent>(i);
-							trans.DisplayOnInspector();
+							
+							if (infoObj.m_parent == -1 || infoObj.m_parent == MAX_ENTITIES)
+							{
+								trans.DisplayOnInspector();
+							}
+							else
+							{
+								//Needs to calculate values from parent objects
+
+								//Parent set
+								Vector2D parentTransform{}, parentScale = trans.GetScale();
+								int parentTransformZ = 0;
+								float parentRotation = 0.0f;
+								HierarchyInfo tempRef = infoObj;
+
+								std::stringstream test;
+								test << "New ParentX: " << parentScale.x << "New ParentY: " << parentScale.y;
+								test << "New ParentX: " << trans.GetScale().x << "New ParentY: " << trans.GetScale().y;
+								while (tempRef.m_parent != -1 && tempRef.m_parent != MAX_ENTITIES)
+								{
+									//Go to next available object and add the new values
+									tempRef = g_engine.m_coordinator.GetHierarchyInfo(tempRef.m_parent);
+
+									TransformComponent& tempTransform = g_engine.m_coordinator.GetComponent<TransformComponent>(tempRef.m_Entity);
+									parentTransform += tempTransform.GetPosition();
+									parentTransformZ += tempTransform.GetZ();
+									parentScale = Vec2(parentScale.x * tempTransform.GetScale().x, parentScale.y * tempTransform.GetScale().y);
+									parentRotation += tempTransform.GetRotation();
+
+
+								}
+
+								//Current set
+								Vector2D transform = trans.GetPosition() - parentTransform, scale = parentScale;
+								int transformZ = trans.GetZ() - parentTransformZ;
+								float rotation = trans.GetRotation() - parentRotation;
+
+								//test << "X: " << scale.x << "Y:" << scale.y << "\n ParentX: " << parentScale.x << "ParentY: " << parentScale.y;
+								RE_INFO(test.str());
+
+								//This section is directly from TransformComponent's DisplayOnInspector with changes. Update this when DisplayOnInspector updates
+								ImGui::Text("Translate");
+								ImGui::SameLine();
+								ImGui::PushItemWidth(75);
+								ImGui::DragFloat("     ", &transform.x);
+								ImGui::SameLine();
+								ImGui::PushItemWidth(75);
+								ImGui::DragFloat("      ", &transform.y);
+
+								ImGui::Text("Z Value  ");
+								ImGui::SameLine();
+								ImGui::DragInt("    ", &transformZ, 1.0f, -100000, 100000);
+
+								if (ImGui::IsItemHovered())
+								{
+									ImGui::BeginTooltip();
+									ImGui::Text("Higher number means the object will be drawn infront");
+									ImGui::EndTooltip();
+								}
+
+								ImGui::Text("Scale    ");
+								ImGui::SameLine();
+								ImGui::PushItemWidth(75);
+								ImGui::DragFloat(" ", &scale.x, 1.0f, 0.0f, 100000.0f);
+								ImGui::SameLine(0.0f, 36.0f);
+								ImGui::DragFloat("  ", &scale.y, 1.0f, 0.0f, 100000.0f);
+
+								ImGui::Text("Rotation ");
+								ImGui::SameLine();
+								ImGui::DragFloat("   ", &rotation, 0.1f, 0.0f, 6.28f);
+
+								//To set values back in
+								trans.setPosition(transform + parentTransform);
+								trans.setZ(transformZ + parentTransformZ);
+								trans.setScale(Vec2(scale.x * parentScale.x, scale.y * parentScale.y));
+								trans.setRotation(rotation + parentRotation);
+
+
+								trans.DisplayOnInspectorWithParent();
+							}
 							if (ImGui::Button("Remove Component"))
 							{
 								g_engine.m_coordinator.RemoveComponent<TransformComponent>(i);
--- RogueEngine/Source/TransformComponent.cpp
+++ RogueEngine/Source/TransformComponent.cpp
@@ -138,18 +138,7 @@ namespace Rogue
 
 	void TransformComponent::DisplayOnInspector()
 	{
-		ImGui::Text("Scale    ");
-		ImGui::SameLine();
-		ImGui::PushItemWidth(75);
-		ImGui::DragFloat(" ", &m_scale.x, 1.0f, 0.0f, 100000.0f);
-		ImGui::SameLine(0.0f, 36.0f);
-		ImGui::DragFloat("  ", &m_scale.y, 1.0f, 0.0f, 100000.0f);
-
-
-		ImGui::Text("Rotation ");
-		ImGui::SameLine();
-		ImGui::DragFloat("   ", &m_rotation, 0.1f, 0.0f, 6.28f);
-		
+		//If this function is updated, update EditorInspector's TransformDisplay also
 		ImGui::Text("Translate");
 		ImGui::SameLine();
 		ImGui::PushItemWidth(75);
@@ -157,35 +146,44 @@ namespace Rogue
 		ImGui::SameLine();
 		ImGui::PushItemWidth(75);
 		ImGui::DragFloat("      ", &m_position.y);
-		ImGui::PushItemWidth(50);
 
-		static char m_priorityDraw[128];
-		ImGui::TextWrapped("Current Z : %d", m_fakeZ);
-		ImGui::TextWrapped("Set Z");
-		ImGui::InputText("                       ", m_priorityDraw, 128);
-		if (ImGui::Button("Set Z"))
-		{
-			m_fakeZ = atoi(m_priorityDraw);
-			setZ(m_fakeZ);
-			memset(m_priorityDraw, 0, 128);
-		}
+		ImGui::Text("Z Value  ");
+		ImGui::SameLine();
+		ImGui::DragInt("    ", &m_fakeZ, 1.0f, -100000, 100000);
+
 		if (ImGui::IsItemHovered())
 		{
 			ImGui::BeginTooltip();
 			ImGui::Text("Higher number means the object will be drawn infront");
 			ImGui::EndTooltip();
 		}
-		
+
+		ImGui::Text("Scale    ");
+		ImGui::SameLine();
+		ImGui::PushItemWidth(75);
+		ImGui::DragFloat(" ", &m_scale.x, 1.0f, 0.0f, 100000.0f);
+		ImGui::SameLine(0.0f, 36.0f);
+		ImGui::DragFloat("  ", &m_scale.y, 1.0f, 0.0f, 100000.0f);
+
+		ImGui::Text("Rotation ");
+		ImGui::SameLine();
+		ImGui::DragFloat("   ", &m_rotation, 0.1f, 0.0f, 6.28f);
+
+		DisplayOnInspectorWithParent();
+	}
+
+	void TransformComponent::DisplayOnInspectorWithParent()
+	{
 		if (ImGui::Button("Reset Position"))
 		{
 			m_position = Vec2{ 0.0f, 0.0f };
 		}
-		
+
 		if (ImGui::Button("Reset Rotation"))
 		{
 			m_rotation = 0.0f;
 		}
-		
+
 		if (ImGui::Button("Reset Scale"))
 		{
 			m_scale = Vec2{ 100.0f,100.0f };
