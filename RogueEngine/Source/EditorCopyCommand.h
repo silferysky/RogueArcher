32d9e0e3467ffe9b1612fe6ec734469f7636dfcf
lohkaiyi <liverpool0_91@hotmail.com>
2020-01-23 11:02:05 +0800

    command WIP

--- RogueEngine/Source/EditorCopyCommand.h
+++ RogueEngine/Source/EditorCopyCommand.h
@@ -2,13 +2,18 @@
 #include "Precompiled.h"
 #include "REEngine.h"
 #include "ICommandable.h"
+#include "Main.h"
 namespace Rogue
 {
 	class EditorCopyCommand : public ICommandable
 	{
-		EditorCopyCommand();
+	public:
+		EditorCopyCommand(Entity entity);
 		~EditorCopyCommand();
 		virtual bool Undo() override;
 		virtual bool Execute() override;
+		Entity GetCopiedEntity() const;
+	private:
+		Entity m_copyEntity = -1;
 	};
 }
