c31127d60112afd9bb6b9df41b0f3390bd4fc5c2
Terence <deathysayshi@gmail.com>
2020-01-28 17:35:48 +0800

    ChildComponent

---
+++ RogueEngine/Source/ChildComponent.cpp
@@ -0,0 +1,55 @@
+#include "ChildComponent.h"
+#include "Precompiled.h"
+
+namespace Rogue
+{
+	//void ChildComponent::SetTransform(Vec2 transform)
+	//{
+	//	m_transform = transform;
+	//}
+
+	//void ChildComponent::SetTransform(float x, float y)
+	//{
+	//	m_transform = Vec2(x, y);
+	//}
+
+	//Vec2 ChildComponent::GetTransform() const
+	//{
+	//	return m_transform;
+	//}
+
+	//void ChildComponent::SetTransformZ(float z)
+	//{
+	//	m_transformZ = z;
+	//}
+
+	//float ChildComponent::GetTransformZ() const
+	//{
+	//	return m_transformZ;
+	//}
+
+	//void ChildComponent::SetScale(Vec2 scale)
+	//{
+	//	m_scale = scale;
+	//}
+
+	//void ChildComponent::SetScale(float x, float y)
+	//{
+	//	m_scale = Vec2(x, y);
+	//}
+
+	//Vec2 ChildComponent::GetScale() const
+	//{
+	//	return m_scale;
+	//}
+
+	//void ChildComponent::SetRotate(float rotate)
+	//{
+	//	m_rotate = rotate;
+	//}
+
+	//float ChildComponent::GetRotate() const
+	//{
+	//	return m_rotate;
+	//}
+}
