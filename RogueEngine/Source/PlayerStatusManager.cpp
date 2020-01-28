9396c5f387f21221674b339f8ecb62e57ad81bd3
Terence <deathysayshi@gmail.com>
2020-01-28 16:20:19 +0800

    Moved teleport related stuff

--- RogueEngine/Source/PlayerStatusManager.cpp
+++ RogueEngine/Source/PlayerStatusManager.cpp
@@ -7,7 +7,10 @@ namespace Rogue
 		m_isLightMode{ true },
 		m_maxJumpTimer{0.5f},
 		m_hitchhikedEntity{ static_cast<Entity>(-1) },
-		m_inLightDur{0.0f}
+		m_inLightDur{0.0f},
+		m_maxTeleportCharge {3.0f},
+		m_teleportCharge{ 3.0f },
+		m_teleportDelayTimer {0.0f}
 	{
 	}
 
@@ -24,6 +27,36 @@ namespace Rogue
 	float PlayerStatusManager::GetInLightDur() const
 	{
 		return m_inLightDur;
+	}
+
+	float PlayerStatusManager::GetTeleportCharge() const
+	{
+		return static_cast<int>(m_teleportCharge);
+	}
+
+	float PlayerStatusManager::GetMaxTeleportCharge() const
+	{
+		return m_maxTeleportCharge;
+	}
+
+	void PlayerStatusManager::IncrementTeleportCharge(float duration)
+	{
+		m_teleportCharge += duration;
+	}
+
+	void PlayerStatusManager::DecrementTeleportDelay(float duration)
+	{
+		m_teleportDelayTimer -= duration;
+	}
+
+	void PlayerStatusManager::SetTeleportDelay(float duration)
+	{
+		m_teleportDelayTimer = duration;
+	}
+
+	float PlayerStatusManager::GetTeleportDelay() const
+	{
+		return m_teleportDelayTimer;
 	}
 
 	void PlayerStatusManager::ToggleLightStatus()
