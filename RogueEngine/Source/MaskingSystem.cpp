83444ba2a03822e3820b0e1cb88e42650b3593aa
Javier <javier_foo@hotmail.com>
2020-01-31 18:40:37 +0800

    Masking

---
+++ RogueEngine/Source/MaskingSystem.cpp
@@ -0,0 +1,90 @@
+#include "Precompiled.h"
+#include "MaskingSystem.h"
+#include "Main.h"
+
+namespace Rogue
+{
+	MaskingSystem::MaskingSystem()
+		: System(SystemID::id_MASKINGSYSTEM)
+	{}
+
+	void MaskingSystem::Init()
+	{
+		REGISTER_LISTENER(SystemID::id_MASKINGSYSTEM, MaskingSystem::Receive);
+
+		// Add components to signature
+		Signature signature;
+		signature.set(g_engine.m_coordinator.GetComponentType<SpriteComponent>());
+		signature.set(g_engine.m_coordinator.GetComponentType<MaskingComponent>());
+
+		// Set graphics system signature
+		g_engine.m_coordinator.SetSystemSignature<MaskingSystem>(signature);
+	}
+
+	void MaskingSystem::Update()
+	{
+		g_engine.m_coordinator.InitTimeSystem("Masking System");
+
+		// For all entities
+		for (auto entity : m_entities)
+		{
+			auto& masking = g_engine.m_coordinator.GetComponent<MaskingComponent>(entity);
+
+			if (masking.getIsActive() == false)
+				continue;
+
+			auto& sprite = g_engine.m_coordinator.GetComponent<SpriteComponent>(entity);
+
+			UpdateTexture(&masking, &sprite);
+		}
+
+		g_engine.m_coordinator.EndTimeSystem("Masking System");
+	}
+
+	void MaskingSystem::UpdateTexture(MaskingComponent* masking, SpriteComponent* sprite)
+	{
+		double timer = (double)masking->getTimer();
+		float maskVelocity = masking->getMaskVelocity();
+		int frameSize = masking->getFrameSize();
+
+		double min = timer * maskVelocity;
+		double max = 1/frameSize + timer + 0.01f * maskVelocity;
+
+		// reset the frame
+		if (max > 1.0)
+		{
+			if (!masking->getIsLooping())
+				masking->setIsActive(false);
+
+			timer = 0.0f;
+
+			masking->setTimer(timer);
+		}
+
+		sprite->setTexCoordMin(static_cast<float>(min));
+		sprite->setTexCoordMax(static_cast<float>(max));
+
+		if (masking->getIsActive())
+			masking->setTimer(timer);
+	}
+
+	void MaskingSystem::ResetTextures()
+	{
+		for (auto entity : m_entities)
+		{
+			auto& masking = g_engine.m_coordinator.GetComponent<MaskingComponent>(entity);
+			auto& sprite = g_engine.m_coordinator.GetComponent<SpriteComponent>(entity);
+
+			sprite.setTexCoordMin(0.0f);
+			sprite.setTexCoordMax(1.0f / masking.getFrameSize());
+		}
+	}
+
+
+
+	void MaskingSystem::Shutdown()
+	{}
+
+	void MaskingSystem::Receive(Event* ev)
+	{}
+}
