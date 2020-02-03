072c02e8b738796ab7c3f16648563b86128a67f1
Terence <deathysayshi@gmail.com>
2020-02-03 19:30:08 +0800

    FadeObject mostly done?

--- RogueEngine/Source/FadeObject.h
+++ RogueEngine/Source/FadeObject.h
@@ -7,25 +7,25 @@ namespace Rogue
 		: public ScriptComponent
 	{
 	public:
-		FadeObject(Entity entity, LogicComponent& logicComponent, StatsComponent& statsComponent, bool isContinuous = false, bool startsByFadingIn = true, float maxTimer = 3.0f);
+		FadeObject(Entity entity, LogicComponent& logicComponent, StatsComponent& statsComponent, bool isContinuous = false, bool active = true, float maxTimer = 3.0f);
 
 		virtual void AIActiveStateUpdate() override;
 		virtual void AIIdleUpdate() override;
 
 		void ResetTimer();
 		void SetMaxTimer(float max);
-		void SetIsFadingIn(bool isFadingIn);
+		void SetIsActive(bool active);
 		void SetContinuous(bool isContinuous);
 
 		float GetTimer() const;
 		float GetMaxTimer() const;
-		bool IsFadingIn() const;
+		bool IsActive() const;
 		bool IsContinuous() const;
 
 	private:
 		float m_timer;
 		float m_maxTimer;
-		bool m_fadingin;
+		bool m_active;
 		bool m_continuous;
 	};
 }
