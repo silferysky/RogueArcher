d1cce42669f54ff27b5a95ca9818c54f22324d52
Javier <javier_foo@hotmail.com>
2020-02-04 22:18:10 +0800

    Point and click to move camera

--- RogueEngine/Source/PickingManager.h
+++ RogueEngine/Source/PickingManager.h
@@ -51,6 +51,8 @@ namespace Rogue
 		void SetViewPortArea(const AABB& aabb);
 		void SetWorldCursor(const Vec2& pos);
 		void SetViewPortCursor(const ImVec2& pos);
+
+		bool isCursorinViewPort() const;
 	};
 
 }
