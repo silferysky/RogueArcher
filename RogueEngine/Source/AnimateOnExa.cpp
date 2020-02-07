ed5906ae38f3fde06b89aee31a2ba8b25adcd3a0
Javier <javier_foo@hotmail.com>
2020-02-07 18:59:44 +0800

    Player dissapears on switch

--- RogueEngine/Source/AnimateOnExa.cpp
+++ RogueEngine/Source/AnimateOnExa.cpp
@@ -25,16 +25,29 @@ namespace Rogue
 			{
 				g_engine.m_coordinator.GetComponent<AnimationComponent>(m_entity).setIsAnimating(true);
 
-				auto& sprite = g_engine.m_coordinator.GetComponent<SpriteComponent>(m_entity);
+				// set parent to disappear
+				Entity m_parent = g_engine.m_coordinator.GetComponent<ChildComponent>(m_entity).GetParent();
+				auto& parentSprite = g_engine.m_coordinator.GetComponent<SpriteComponent>(m_parent);
+				glm::vec4 parentFilter = parentSprite.getFilter();
+				parentFilter.a = 0.0f;
+				parentSprite.setFilter(parentFilter);
 
+				auto& sprite = g_engine.m_coordinator.GetComponent<SpriteComponent>(m_entity);
 				glm::vec4 colourFilter = sprite.getFilter();
 				colourFilter.a = 1.0f;
-				g_engine.m_coordinator.GetComponent<SpriteComponent>(m_entity).setFilter(colourFilter);
+				sprite.setFilter(colourFilter);
 			}
 		}
 		else if (!g_engine.m_coordinator.GetComponent<AnimationComponent>(m_entity).getIsAnimating() && g_engine.m_coordinator.GetComponent<SpriteComponent>(m_entity).getFilter().a)
 			// not animating and not transparent
 		{
+			//set parent to appear
+			Entity m_parent = g_engine.m_coordinator.GetComponent<ChildComponent>(m_entity).GetParent();
+			auto& parentSprite = g_engine.m_coordinator.GetComponent<SpriteComponent>(m_parent);
+			glm::vec4 parentFilter = parentSprite.getFilter();
+			parentFilter.a = 1.0f;
+			parentSprite.setFilter(parentFilter);
+
 			glm::vec4 colourFilter = g_engine.m_coordinator.GetComponent<SpriteComponent>(m_entity).getFilter();
 			colourFilter.a = 0.0f;
 			g_engine.m_coordinator.GetComponent<SpriteComponent>(m_entity).setFilter(colourFilter);
