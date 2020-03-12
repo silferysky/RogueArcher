b9e00865178b842cf45a3a2eb63d1545499a871e
Javier <javier_foo@hotmail.com>
2020-03-13 02:59:40 +0800

    breaking

--- RogueEngine/Source/VegetationBreakable.cpp
+++ RogueEngine/Source/VegetationBreakable.cpp
@@ -1,6 +1,7 @@
 #include "Precompiled.h"
 #include "VegetationBreakable.h"
 #include "PlayerStatusManager.h"
+#include "GraphicsEvent.h"
 //#include "Main.h"	//For g_deltaTime and coordinator
 
 namespace Rogue
@@ -15,6 +16,10 @@ namespace Rogue
 		if (other == PLAYER_STATUS.GetHitchhikedEntity()) // if collided into by the hitchhiked entity
 		{
 			g_engine.m_coordinator.GetComponent<RigidbodyComponent>(m_entity).setIsStatic(false); // switch off the rigidbody
+
+			CameraShakeEvent shake(5.0f);
+			shake.SetSystemReceivers(static_cast<int>(SystemID::id_CAMERASYSTEM));
+			EventDispatcher::instance().AddEvent(shake);
 		}
 	}
 }
