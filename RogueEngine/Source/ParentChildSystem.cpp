b3e8f0c63b732b7d541105d60848e169073fbbe2
Terence Chan <deathysayshi@gmail.com>
2020-03-13 01:32:12 +0800

    Update ParentChildSystem.cpp

--- RogueEngine/Source/ParentChildSystem.cpp
+++ RogueEngine/Source/ParentChildSystem.cpp
@@ -23,76 +23,7 @@ namespace Rogue
 	{
 		for (Entity entity : m_entities)
 		{
-			auto& childComponent = g_engine.m_coordinator.GetComponent<ChildComponent>(entity);
-
-			if (childComponent.GetParent() == MAX_ENTITIES || !g_engine.m_coordinator.ComponentExists<TransformComponent>(childComponent.GetParent()))
-				return;
-
-			auto& transComponent = g_engine.m_coordinator.GetComponent<TransformComponent>(entity);
-			auto& parentTransformComponent = g_engine.m_coordinator.GetComponent<TransformComponent>(childComponent.GetParent());
-
-			if (entity == PLAYER_STATUS.GetPlayerEntity())
-				PLAYER_STATUS.SetTeleportCharge(1);
-
-			//Local values is "corrupted", need to fix
-			if (childComponent.IsLocalDirty())
-			{
-				childComponent.SetPosition(transComponent.GetPosition() - parentTransformComponent.GetPosition());
-				childComponent.SetPositionZ(transComponent.GetZ() - parentTransformComponent.GetZ());
-				childComponent.SetScale(Vec2(transComponent.GetScale().x / parentTransformComponent.GetScale().x, transComponent.GetScale().y / parentTransformComponent.GetScale().y));
-				childComponent.SetRotation(transComponent.GetRotation() - parentTransformComponent.GetRotation());
-				//If grandparent don't exists
-				//g_engine.m_coordinator.GetComponent<ChildComponent>(childComponent.GetParent()).GetParent() == MAX_ENTITIES;
-				//if (!g_engine.m_coordinator.ComponentExists<ChildComponent>(childComponent.GetParent()))
-				//{
-				//	//Calculate position based on global transform
-				//	//No need to do in depth value change because no extra parent to calculate
-				//	childComponent.SetPosition(transComponent.GetPosition() - parentTransformComponent.GetPosition());
-				//	childComponent.SetPositionZ(transComponent.GetZ() - parentTransformComponent.GetZ());
-				//	childComponent.SetScale(Vec2(transComponent.GetScale().x / parentTransformComponent.GetScale().x, transComponent.GetScale().y / parentTransformComponent.GetScale().y));
-				//	childComponent.SetRotation(transComponent.GetRotation() - parentTransformComponent.GetRotation());
-				//}
-				//else
-				//{
-				//	childComponent.SetPosition(transComponent.GetPosition() - parentTransformComponent.GetPosition());
-				//	childComponent.SetPositionZ(transComponent.GetZ() - parentTransformComponent.GetZ());
-				//	childComponent.SetScale(Vec2(transComponent.GetScale().x / parentTransformComponent.GetScale().x, transComponent.GetScale().y / parentTransformComponent.GetScale().y));
-				//	childComponent.SetRotation(transComponent.GetRotation() - parentTransformComponent.GetRotation());
-				//}
-				childComponent.ResetLocalDirty();
-			}
-
-			//Global values is "corrupted", need to fix
-			else if (childComponent.IsGlobalDirty())
-			{
-				transComponent.setPosition(childComponent.GetPosition() + parentTransformComponent.GetPosition());
-				transComponent.setZ(childComponent.GetPositionZ() + parentTransformComponent.GetZ());
-				transComponent.setScale(Vec2(childComponent.GetScale().x * parentTransformComponent.GetScale().x, childComponent.GetScale().y * parentTransformComponent.GetScale().y));
-				transComponent.setRotation(childComponent.GetRotation() + parentTransformComponent.GetRotation());
-
-				std::vector<Entity> toUpdate;
-				AddChildToVector(toUpdate, entity);
-				for (auto& ent : toUpdate)
-					g_engine.m_coordinator.GetComponent<ChildComponent>(ent).SetGlobalDirty();
-
-				toUpdate.clear();
-
-				childComponent.ResetGlobalDirty();
-			}
-			//Following only happens if no local or global changes, but you just want to update it
-			else if (childComponent.IsFollowing())
-			{
-				transComponent.setPosition(childComponent.GetPosition() + parentTransformComponent.GetPosition());
-				transComponent.setZ(childComponent.GetPositionZ() + parentTransformComponent.GetZ());
-				transComponent.setScale(Vec2(childComponent.GetScale().x * parentTransformComponent.GetScale().x, childComponent.GetScale().y * parentTransformComponent.GetScale().y));
-				transComponent.setRotation(childComponent.GetRotation() + parentTransformComponent.GetRotation());
-			}
-
-			if (transComponent.GetScale().x == 0.0f)
-				transComponent.setScale(Vec2(1.0f, transComponent.GetScale().y));
-			if (transComponent.GetScale().y == 0.0f)
-				transComponent.setScale(Vec2(transComponent.GetScale().x, 1.0f));
-
+			ApplyParentChildTransform(entity);
 		}
 	}
 
