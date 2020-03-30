220e8574468f2a6188cf2e10bf58c7470b138d4f
Javier <javier_foo@hotmail.com>
2020-03-31 07:18:41 +0800

    Camera now follows when pillbug drops

--- RogueEngine/Source/VegetationBreakable.cpp
+++ RogueEngine/Source/VegetationBreakable.cpp
@@ -7,8 +7,30 @@
 namespace Rogue
 {
 	VegetationBreakable::VegetationBreakable(Entity entity, LogicComponent& logicComponent, StatsComponent& statsComponent)
-		: ScriptComponent(entity, logicComponent, statsComponent)
+		: ScriptComponent(entity, logicComponent, statsComponent), m_triggered{false}
 	{
+	}
+
+	void VegetationBreakable::AIActiveStateUpdate()
+	{
+		m_logicComponent->SetActiveStateBit(static_cast<size_t>(AIState::AIState_Idle));
+	}
+
+	void VegetationBreakable::AIIdleUpdate()
+	{
+		if (!m_triggered || !g_engine.m_coordinator.GameIsActive() || PLAYER_STATUS.GetPlayerEntity() == MAX_ENTITIES)
+			return;
+
+		// Once death timer has reached lifetime
+		if (m_timer > 2.0f)
+		{
+			PLAYER_STATUS.UnfreezeControls();
+			g_engine.m_coordinator.GetSystem<CameraSystem>()->setIsActive(true);
+			m_triggered = false;
+		}
+
+		// Increment age by deltatime
+		m_timer += g_deltaTime * g_engine.GetTimeScale();
 	}
 
 	void VegetationBreakable::OnTriggerEnter(Entity other)
@@ -22,6 +44,7 @@ namespace Rogue
 			shake.SetSystemReceivers(static_cast<int>(SystemID::id_CAMERASYSTEM));
 			EventDispatcher::instance().AddEvent(shake);
 			g_engine.m_coordinator.GetSystem<CameraSystem>()->setIsActive(false);
+			m_triggered = true;
 		}
 	}
 }
--- RogueEngine/Source/VegetationBreakable.h
+++ RogueEngine/Source/VegetationBreakable.h
@@ -9,6 +9,12 @@ namespace Rogue
 	public:
 		VegetationBreakable(Entity entity, LogicComponent& logicComponent, StatsComponent& statsComponent);
 
+		virtual void AIActiveStateUpdate() override;
+		virtual void AIIdleUpdate() override;
+
 		virtual void OnTriggerEnter(Entity other) override;
+	private:
+		float m_timer = 0.0f;
+		bool m_triggered = false;
 	};
 }
